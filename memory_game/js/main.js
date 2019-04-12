d38fdb2077e8bcd32d0483ad6cae4af5a95deaa3
Calvin Féau <calvin@Calvins-MacBook-Pro.local>
2019-04-12 12:05:29 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,18 @@
-console.log("Up and running!");
+cards = ["queen", "queen", "king", "king"];
+cardsInPlay = [];
+cardOne = cards[0];
+cardTwo = cards[1];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
 
-console.log("User flipped " + cardThree);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+}
